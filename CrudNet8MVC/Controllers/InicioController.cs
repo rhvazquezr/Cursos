f91dc88231e5b9228c9fa585f917c504871facd2
rhvazquezr <rhvazquezr@hotmail.com>
2023-10-28 15:31:09 -0700

    Fin del Curso

--- CrudNet8MVC/Controllers/InicioController.cs
+++ CrudNet8MVC/Controllers/InicioController.cs
@@ -15,6 +15,7 @@ namespace CrudNet8MVC.Controllers
             _contexto = contexto;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _contexto.Contactos.ToListAsync());
@@ -27,6 +28,7 @@ namespace CrudNet8MVC.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Crear(Contacto contacto)
         {
             if (ModelState.IsValid)
@@ -37,9 +39,86 @@ namespace CrudNet8MVC.Controllers
             }
             return View();
         }
-        public IActionResult Privacy()
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+            var contacto = await _contexto.Contactos.FindAsync(id);
+            if (contacto == null)
+            {
+                return View();
+            }
+
+            //Borrado
+            _contexto.Contactos.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
